25e17aa9d9288639a5eef83ddd338143a1b125f7
Shahid Khan <shahidjoura307@gmail.com>
2025-12-13 22:03:22 +0530

    api

--- github-profile/src/App.jsx
+++ github-profile/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React ,{useState, useEffect} from 'react'
+import axios from 'axios'
+import Card from './Card'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [data, setdata] = useState([])
+  const getData = async()=>{
+      const res = await axios.get(`https://api.github.com/users?per_page=10`)
+      setdata(res.data)
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    getData()
+  }, [])
+  
+  console.log(data)
+
+return (
+  <div className='flex gap-2 flex-wrap'>
+    {data.map((e, index) => (
+      <Card key={index} data={e} />
+    ))}
+  </div>
+);
 }
 
 export default App
