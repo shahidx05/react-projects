7c97de7c311745c2008cbafc03b92d1d89490d0e
Shahid Khan <shahidjoura307@gmail.com>
2025-12-14 21:09:01 +0530

    done

--- github-profile/src/App.jsx
+++ github-profile/src/App.jsx
@@ -1,27 +1,49 @@
-import React ,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Card from './Card'
 
 const App = () => {
   const [data, setdata] = useState([])
-  const getData = async()=>{
-      const res = await axios.get(`https://api.github.com/users?per_page=10`)
-      setdata(res.data)
+  const [num, setnum] = useState("10")
+  const [loading, setLoading] = useState(false)
+
+  const getData = async () => {
+    if (num <= 0) return
+    setLoading(true)
+    const res = await axios.get(`https://api.github.com/users?per_page=${num}`)
+    setdata(res.data)
+    
+    setLoading(false)
   }
 
   useEffect(() => {
     getData()
   }, [])
-  
+
   console.log(data)
 
-return (
-  <div className='flex gap-2 flex-wrap'>
-    {data.map((e, index) => (
-      <Card key={index} data={e} />
-    ))}
-  </div>
-);
+  return (
+    <>
+      <div className='my-10 mx-auto w-xs flex gap-2'>
+        <input className='border-2 p-1.5 rounded-xl'value={num} type="number" onChange={(e) => {
+          setnum(e.target.value)
+        }} />
+        <button className='bg-amber-200 p-2.5 rounded-2xl' onClick={getData} >Get Data</button>
+      </div>
+      {loading && (
+        <p className="text-center text-lg font-medium">
+          Loading...
+        </p>
+      )}
+      {!loading && (
+        <div className='flex gap-2 flex-wrap'>
+          {data.map(user => (
+            <Card key={user.id} data={user} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App
--- github-profile/src/Card.jsx
+++ github-profile/src/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({data}) => {
   return (
     <div className='w-60 border-2 flex flex-col items-center p-5 gap-3 rounded-2xl'  >
-        <img className='w-50' src={data.avatar_url} alt=""  />
+        <img className='w-50 rounded-full '  src={data.avatar_url} alt=""  />
         <h1 className='text-3xl font-medium' >{data.login}</h1>
         <a className='font-normal text-blue-800' href={data.html_url} target="_blank" >Get Profile</a>
     </div>
