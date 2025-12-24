7687a2c3123b2b61e58d676a9aaaf796e2573880
Shahid Khan <shahidjoura307@gmail.com>
2025-12-24 19:54:04 +0530

    80%

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import Layout from './Layout.jsx'
+import About from './components/About.jsx'
+import Home from './components/Home.jsx'
+import Contact from './components/Contact.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>} >
+      <Route path='' element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='contact' element={<Contact/>} />
+    </Route>
+
+  )
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
