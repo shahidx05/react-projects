b84b1dce61e32f6226154bddd4efcedd17c96e6d
Shahid Khan <shahidjoura307@gmail.com>
2025-12-25 20:30:29 +0530

    fix

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -7,6 +7,7 @@ import Layout from './Layout.jsx'
 import About from './components/About.jsx'
 import Home from './components/Home.jsx'
 import Contact from './components/Contact.jsx'
+import Github from './components/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,6 +15,7 @@ const router = createBrowserRouter(
       <Route path='' element={<Home/>} />
       <Route path='about' element={<About/>} />
       <Route path='contact' element={<Contact/>} />
+      <Route path='github/:id' element={<Github/>} />
     </Route>
 
   )
