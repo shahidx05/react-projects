bfffe3297e75485a8877b11a73314ce1f9aa580d
Shahid Khan <shahidjoura307@gmail.com>
2026-04-01 21:30:28 +0530

    gh prof

---
+++ github-profile/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="bg-gray-900 text-white py-4 shadow-md">
+      <h1 className="text-center text-2xl font-bold tracking-wide">
+        GitHub User Explorer 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default Header
--- github-profile/src/App.jsx
+++ github-profile/src/App.jsx
@@ -1,31 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Card from './Card'
+import Card from '../components/Card'
+import Header from '../components/Header'
 
 const App = () => {
   const [data, setdata] = useState([])
-  const [num, setnum] = useState("10")
+  const [num, setnum] = useState(10)
   const [loading, setLoading] = useState(false)
 
   const getData = async () => {
     if (num <= 0) return
     setLoading(true)
-    const res = await axios.get(`https://api.github.com/users?per_page=${num}`)
-    setdata(res.data)
-    
-    setLoading(false)
+    try {
+      const res = await axios.get(`https://api.github.com/users?per_page=${num}`)
+      setdata(res.data)
+      console.log("hello")
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   useEffect(() => {
     getData()
   }, [])
 
-  console.log(data)
-
   return (
     <>
+    <Header/>
       <div className='my-10 mx-auto w-xs flex gap-2'>
-        <input className='border-2 p-1.5 rounded-xl'value={num} type="number" onChange={(e) => {
+        <input className='border-2 p-1.5 rounded-xl' value={num} type="number" onChange={(e) => {
           setnum(e.target.value)
         }} />
         <button className='bg-amber-200 p-2.5 rounded-2xl' onClick={getData} >Get Data</button>
